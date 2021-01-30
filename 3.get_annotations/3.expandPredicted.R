fbaf6a1c09eca68716a263e16c61879e39b96092
Wimalanathan Kokulapalan <wkpalan@users.noreply.github.com>
2021-01-30 08:01:32 -0500

    adding the lastest changes to expanding annotations and removing expanded files from git tracking

--- 3.get_annotations/3.expandPredicted.R
+++ 3.get_annotations/3.expandPredicted.R
@@ -1,9 +1,9 @@
-source("analysis/R/gafTools.R")
-source("analysis/R/oboTools.R")
+source("R/gafTools.R")
+source("R/oboTools.R")
 library("data.table")
 library("yaml")
 
-config = read_yaml("config.yml")
+config = read_yaml("config.yaml")
 go_obo <- check_obo_data("data/go/go.obo")
 
 getAllGo <- function(go_obo,x){
@@ -18,7 +18,7 @@ getAllGo <- function(go_obo,x){
 
 predData_list <- mclapply(config$predicted,function(x){
   print(x)
-  inputGaf = file.path("data","clean",x)
+  inputGaf = file.path("data","clean",x$file)
   input_gaf_data <- read_gaf(inputGaf)
   input_gaf_data[,qualifier:=0]
   input_gaf_data[,with:=0]
@@ -27,7 +27,7 @@ predData_list <- mclapply(config$predicted,function(x){
   output_gaf_data <- unique(input_gaf_data[,list(term_accession=getAllGo(go_obo,term_accession)),by=eval(gaf_cols[!gaf_cols %in% "term_accession"])])
   today=format(Sys.Date(),"%Y%m%d")
   output_gaf_data[,date:=as.numeric(today)]
-  outputGaf = file.path("data","expanded",x)
+  outputGaf = file.path("data","expanded",x$file)
   write_gaf(output_gaf_data,outputGaf)
   outputGaf
 },mc.cores = 4)
