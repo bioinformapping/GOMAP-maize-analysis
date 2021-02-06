f552764b231fe620c0193b44eae109c731b381c4
Wimalanathan Kokulapalan <wkpalan@users.noreply.github.com>
2021-02-06 12:03:51 -0500

    added the performance optimized code to expand gold

--- 3.get_annotations/3.expandPredicted.R
+++ 3.get_annotations/3.expandPredicted.R
@@ -7,7 +7,7 @@ config = read_yaml("config.yaml")
 go_obo <- check_obo_data("data/go/go.obo")
 
 getAllGo <- function(go_obo,x){
-  allGo = unique(unlist(go_obo$ancestors[x]))
+  allGo = go_obo$ancestors[[x]]
   if(!is.null(allGo)){
     allGo = allGo
   }else{
@@ -16,19 +16,26 @@ getAllGo <- function(go_obo,x){
   allGo
 }
 
-predData_list <- mclapply(config$predicted,function(x){
-  print(x)
+
+predData_list <- lapply(config$predicted,function(x){
+  print(x["inbred"])
   inputGaf = file.path("data","clean",x$file)
   input_gaf_data <- read_gaf(inputGaf)
   input_gaf_data[,qualifier:=0]
   input_gaf_data[,with:=0]
   input_gaf_data[,db_object_name:=""]
   input_gaf_data[,db_object_synonym:=""]
-  output_gaf_data <- unique(input_gaf_data[,list(term_accession=getAllGo(go_obo,term_accession)),by=eval(gaf_cols[!gaf_cols %in% "term_accession"])])
+  unique_terms = unique(input_gaf_data[,.(aspect,term_accession)])
+  all_terms = unique_terms[,list(all_term_accession=unique(go_obo$ancestors[[term_accession]])),by=list(aspect,term_accession)]
+  expand_gaf_data = unique(merge.data.table(input_gaf_data,all_terms,allow.cartesian = T,all.x = T))
+  expand_gaf_data[is.na(all_term_accession),all_term_accession:=term_accession]
+  output_gaf_data = unique(expand_gaf_data[,-c("term_accession"),with=F])
+  colnames(output_gaf_data)[colnames(output_gaf_data)=="all_term_accession"] = "term_accession"
+  setcolorder(output_gaf_data,gaf_cols)
   today=format(Sys.Date(),"%Y%m%d")
   output_gaf_data[,date:=as.numeric(today)]
   outputGaf = file.path("data","expanded",x$file)
   write_gaf(output_gaf_data,outputGaf)
-  outputGaf
-},mc.cores = 4)
+  NULL
+})
 
